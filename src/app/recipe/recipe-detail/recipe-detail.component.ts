ea19636088a70a90f7a5282cfa285f2ad332856e
Bruno Staub <bruno.staub@gmail.com>
2018-07-27 23:59:03 +0200

    Ende Unit 94, Routing

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 import {ShoppingListService} from '../../shopping-list/shopping-list.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,20 +11,29 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class RecipeDetailComponent implements OnInit {
   selectedRecipe: Recipe;
+  recipeId: number;
 
   constructor(private recipeService: RecipeService,
               private shoppingListServce: ShoppingListService,
-              private activatedRoute: ActivatedRoute
+              private activatedRoute: ActivatedRoute,
+              private router: Router
   ) { }
 
-  ngOnInit() {
+  ngOnInit() { // zieh das aus URL ist alles Text mit + mache ich wieder number draus
     this.activatedRoute.params.subscribe(
-      params => this.selectedRecipe = this.recipeService.getRecipe(+params['id'])  // zieh das aus URL ist alles Text mit + mache ich wieder number draus
+      params => {
+        this.recipeId = +params['id']
+        this.selectedRecipe = this.recipeService.getRecipe(this.recipeId);
+      }
     );
   }
 
   onAddToList() {
     this.shoppingListServce.addIncredients(this.selectedRecipe.incredients);
+  }
+
+  onEdit() {
+    this.router.navigate(['/rezepte', this.recipeId, 'bearbeiten']); // Full Path muss übergeben werden!
   }
 
 }
--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,10 +11,14 @@ import {RecipeService} from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) {  }
+  constructor(private recipeService: RecipeService, private router: Router) {  }
 
   ngOnInit() {  // Aufgaben die etwas länger dauern könnten nicht in constructor, sondern in ngOnInit reintun!!!
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['/rezepte', 'neu']);
   }
 
 }
